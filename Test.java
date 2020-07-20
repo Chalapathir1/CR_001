450753efd730cfc6326a9c85b3a355320238378b
Chalapathir1 <rchalapaty@gamil.com>
2020-07-20 06:30:35 +0000

    modified for webhooks

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
   public static void main(String[] args) {
       System.out.println("Hello, Test");
       System.out.println("Test message");     
-	System.out.println("hi ");
+	System.out.println("hi Devops ");
   }
 }
 
